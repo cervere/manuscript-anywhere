dce61584a864cd75c12ffcca2574d4801e009c25
cervere <bhargav.rrv@gmail.com>
2023-10-09 17:09:04 -0400

    remove unnecessary

--- src/App.js
+++ src/App.js
@@ -16,17 +16,6 @@ const App = () => {
     const collapsedClass = "collapsed";
     const lightModeClass = "light-mode";
     
-    // /*TOGGLE HEADER STATE*/
-    // collapseBtn.addEventListener("click", function () {
-    //   console.log('Collapse')
-    //   body.classList.toggle(collapsedClass);
-    //   this.getAttribute("aria-expanded") == "true"
-    //     ? this.setAttribute("aria-expanded", "false")
-    //     : this.setAttribute("aria-expanded", "true");
-    //   this.getAttribute("aria-label") == "collapse menu"
-    //     ? this.setAttribute("aria-label", "expand menu")
-    //     : this.setAttribute("aria-label", "collapse menu");
-    // });
     
     /*TOGGLE MOBILE MENU*/
     toggleMobileMenu.addEventListener("click", function () {
@@ -75,7 +64,7 @@ const App = () => {
   }, []); // Empty dependency array means this effect runs once after mounting
 
   return (
-    <DashboardLayout></DashboardLayout>
+    <DashboardLayout />
   )
 };
 
